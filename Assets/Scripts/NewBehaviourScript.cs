b3d6804e3a60014d9e2f27ead75d8df308c912cc
Ruihan Wang <harrywangshc@gmail.com>
2018-05-15 21:31:31 -0400

    Initial commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public bool doSpin = true;
+    public float spinSpeed = 180.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
